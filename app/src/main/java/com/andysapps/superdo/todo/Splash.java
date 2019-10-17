c78794d4a08d5b9a9826e962465ac2adb64ca00d
Andrews Alves <andrewsalvisbusiness@gmail.com>
2019-10-17 08:51:44 +0530

    animation codes

--- app/src/main/java/com/andysapps/superdo/todo/Splash.java
+++ app/src/main/java/com/andysapps/superdo/todo/Splash.java
@@ -2,13 +2,50 @@ package com.andysapps.superdo.todo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+import com.andysapps.superdo.todo.activity.MainActivity;
+import com.daimajia.androidanimations.library.Techniques;
+import com.daimajia.androidanimations.library.YoYo;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Splash extends AppCompatActivity {
 
+    @BindView(R.id.tv_plan)
+    TextView tvPlan;
+
+    @BindView(R.id.tv_prioritize)
+    TextView tvPrio;
+
+    @BindView(R.id.tv_takeaction)
+    TextView tvTakeAction;
+
+
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+
+        YoYo.with(Techniques.FadeInLeft).duration(500).repeat(1).playOn(tvPlan);
+        YoYo.with(Techniques.FadeInDown).duration(500).repeat(1).delay(500).playOn(tvPlan);
+        YoYo.with(Techniques.FadeInRight).duration(500).repeat(1).delay(1000).playOn(tvPlan);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+            }
+        }, 2500);
+
     }
 }
