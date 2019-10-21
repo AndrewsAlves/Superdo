173095f8ccd45d09bc835a442964834d50ce999d
AndrewsAlvis <andrewsalvisbusiness@gmail.com>
2019-10-21 22:05:15 +0530

    improvments

--- app/src/main/java/com/andysapps/superdo/todo/fragments/BucketFragment.java
+++ app/src/main/java/com/andysapps/superdo/todo/fragments/BucketFragment.java
@@ -4,6 +4,8 @@ package com.andysapps.superdo.todo.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,22 +13,37 @@ import android.view.ViewGroup;
 
 import com.andysapps.superdo.todo.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class BucketFragment extends Fragment {
 
-
     public BucketFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bucket, container, false);
+        View v = inflater.inflate(R.layout.fragment_bucket, container, false);
+        ButterKnife.bind(this, v);
+
+        return v;
+    }
+
+    @OnClick(R.id.ib_close)
+    public void clickClose() {
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+
+        if (fragmentManager.findFragmentById(R.id.fl_bucket_fragment_container) != null) {
+
+            fragmentManager.beginTransaction().
+                    remove(fragmentManager.findFragmentById(R.id.fl_bucket_fragment_container))
+                    .commitAllowingStateLoss();
+        }
     }
 
 }
--- app/src/main/java/com/andysapps/superdo/todo/fragments/TasksFragment.java
+++ app/src/main/java/com/andysapps/superdo/todo/fragments/TasksFragment.java
@@ -4,6 +4,7 @@ package com.andysapps.superdo.todo.fragments;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,10 +12,14 @@ import android.view.ViewGroup;
 
 import com.andysapps.superdo.todo.R;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 /**
  * A simple {@link Fragment} subclass.
  */
 public class TasksFragment extends Fragment {
+
 
 
     public TasksFragment() {
@@ -26,7 +31,18 @@ public class TasksFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_tasks, container, false);
+        ButterKnife.bind(this, v);
+
+
         return v;
+    }
+
+    @OnClick(R.id.ib_buckets)
+    public void clickBuckets() {
+        BucketFragment fragment = new BucketFragment();
+        FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.fl_bucket_fragment_container, fragment);
+        ft.commitAllowingStateLoss(); // save the changes
     }
 
 }
