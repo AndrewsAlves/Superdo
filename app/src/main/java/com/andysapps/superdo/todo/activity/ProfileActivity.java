48368935c624da840e6b02594f4efc1379303c61
AndrewsAlvis <andrewsalvisbusiness@gmail.com>
2019-10-24 15:58:29 +0530

    xml layouts

---
+++ app/src/main/java/com/andysapps/superdo/todo/activity/ProfileActivity.java
@@ -0,0 +1,16 @@
+package com.andysapps.superdo.todo.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.andysapps.superdo.todo.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
